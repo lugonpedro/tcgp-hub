f837680e0a1ee00286d7bd8e289db04bfd95730e
Pedro Lugon <pedromotta20@gmail.com>
2024-12-03 13:43:03 -0300

    feat: get data from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from "react";
+import { getJSON } from "./utils/get-json";
 
 function App() {
+  const [pokes, setPokes] = useState<any[]>([]);
+
+  useEffect(() => {
+    getJSON(
+      "https://raw.githubusercontent.com/chase-manning/pokemon-tcg-pocket-cards/refs/heads/main/v1.json",
+      function (err, data) {
+        if (err) {
+          console.error(err)
+          return
+        }
+
+        setPokes(data)
+      }
+    );
+  }, []);
+
   return (
     <>
-    <p className='text-3xl'>TCGP Hub by Pedro Lugon</p>
+      <h1 className="text-3xl">TCGP Hub by Pedro Lugon</h1>
+      {pokes.map((poke) => (
+        <p>{poke.name}</p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
