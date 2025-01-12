7d65a51ff0a01b35894d6ee9bf8a1ad729b304ab
Pedro Lugon <pedromotta20@gmail.com>
2025-01-12 01:22:07 -0300

    feat: add loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,5 +1,16 @@
+import { motion } from "motion/react";
+
 export default function Loading() {
-  return(
-    <p className="text-2xl text-background">Loading...</p>
-  )
+  return (
+    <motion.img
+      src="flip.png"
+      className="h-16 w-16 md:h-32 md:w-32"
+      animate={{ rotate: 360 }}
+      transition={{
+        repeat: Infinity,
+        repeatType: "loop",
+        duration: 2,
+      }}
+    />
+  );
 }
