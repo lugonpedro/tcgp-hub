e44c584ba0c95bb1b3b9f31da15c92cae291159d
Pedro Lugon <pedromotta20@gmail.com>
2025-01-12 22:45:22 -0300

    feat: add jump to start and end in paginator

--- src/components/paginator.tsx
+++ src/components/paginator.tsx
@@ -13,6 +13,13 @@ export default function Paginator({ page, setPage, cards, pageLimit }: Paginator
       <Button
         className="bg-secondary text-black hover:bg-secondary/80"
         disabled={page === 1}
+        onClick={() => setPage(1)}
+      >
+        &#60;&#60;
+      </Button>
+      <Button
+        className="bg-secondary text-black hover:bg-secondary/80"
+        disabled={page === 1}
         onClick={() => setPage(page - 1)}
       >
         &#60;
@@ -28,6 +35,13 @@ export default function Paginator({ page, setPage, cards, pageLimit }: Paginator
         onClick={() => setPage(page + 1)}
       >
         &#62;
+      </Button>
+      <Button
+        className="bg-secondary text-black hover:bg-secondary/80"
+        disabled={page === Math.floor(cards.length / pageLimit) + 1}
+        onClick={() => setPage(Math.floor(cards.length / pageLimit) + 1)}
+      >
+        &#62;&#62;
       </Button>
     </div>
   );
